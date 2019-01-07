56eeed6a9f7155c9a06f0a57eda2529a2da6a940
Shuhao Tan <johnmave126@gmail.com>
2019-01-07 20:08:38 +0800

    Bump version

--- beatmapml_gpu/__init__.py
+++ beatmapml_gpu/__init__.py
@@ -1,6 +1,6 @@
 from .snapshot import make_snapshots
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 __all__ = [
     'make_snapshots'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='beatmapml_gpu',
-    version='0.1.5',
+    version='0.1.6',
     description=('Utilities for osu! beatmap in machine '
                  'learning, GPU accelerated'),
     author='Youmu Chan',
